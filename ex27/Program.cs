136e2e617cddcae278d6469d44caa5c030ab6327
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-24 21:48:31 +0300

    Выполнил задачу 27

---
+++ ex27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27. Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int ReadIntAbs(string text)
+{
+    System.Console.WriteLine(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int SumNumbers(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
+
+int number = ReadIntAbs("Введите число: ");
+System.Console.WriteLine(SumNumbers(number));
