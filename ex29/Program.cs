b1cb5aecbc2834fd7f0cdeaafdaefa9ab7116368
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-24 22:23:36 +0300

    Выполнил задачу 29

---
+++ ex29/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 29. Напишите программу, которая создаёт массив заданной длины,
+// заполняет его случайными числами заданного диапазона и выводит его на
+// экран.
+
+int ReadIntAbs(string text)
+{
+    System.Console.WriteLine(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+
+void FillArray(int[] arrayToFill, int down, int up)
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < arrayToFill.Length; i++)
+    {
+        arrayToFill[i] = rand.Next(down, up);
+    }
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        System.Console.Write(arrayForPrint[i] + " ");
+    }
+}
+
+int lengthArray = ReadIntAbs("Введите длину массива: ");
+int downRand = ReadIntAbs("Введите Нижнюю границу генерации чисел массива: ");
+int topRand = ReadIntAbs("Введите Верхнюю границу генерации чисел массива: ");
+
+int[] array = new int[lengthArray];
+
+FillArray(array, downRand, topRand);
+PrintArray(array);
